00cb820864f0b8517b6f2864b20fabf60e263022
mokainos <72137340+mokainos@users.noreply.github.com>
2021-03-19 13:12:07 +0000

    Update lint-yaml.sh
    removed reference to dirname

--- tests/lint-yaml.sh
+++ tests/lint-yaml.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 set -ex
 
-cd ../
-workdir=$(dirname $0)
-yamllint -c $workdir/.yamllint.yaml $(find . -not -path '*/\.*' -type f -name '*.yaml')
+yamllint -c .yamllint.yaml $(find . -not -path '*/\.*' -type f -name '*.yaml')
