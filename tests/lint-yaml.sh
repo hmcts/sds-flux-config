54874d06e5b98366e7818c5af50a6f5cddb7d533
Tim Jacomb <tim.jacomb@hmcts.net>
2021-03-19 15:09:49 +0000

    Ignore charts directory

--- tests/lint-yaml.sh
+++ tests/lint-yaml.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -ex
 
-yamllint -c .yamllint.yaml $(find . -not -path '*/\.*' -type f -name '*.yaml')
+yamllint -c .yamllint.yaml $(find . -not -path '*/\.*' -not -path '**/charts/**' -type f -name '*.yaml')
