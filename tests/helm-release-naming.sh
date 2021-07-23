a14ad8ab2c8078e711dd4673ef1bd5d39e3b0ccb
msl8r <72858053+msl8r@users.noreply.github.com>
2021-07-23 13:32:54 +0100

    Update namespace check (#1055)
    * Fix namespace check
    
    * Update yq to 4.11.1
    
    * Update install-yq.sh
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * tidy up

--- tests/helm-release-naming.sh
+++ tests/helm-release-naming.sh
@@ -2,14 +2,13 @@
 set -e
 
 for file in $(grep -lr "kind: HelmRelease" --exclude-dir={admin,monitoring,neuvector,azure-devops,kube-system,jenkins} "k8s/namespaces"); do
-  
   NAMESPACE="$( echo $file | cut -d'/' -f3)"
-  FILE_NAMESPACE=$(yq eval '{.metadata.name:.[.name]}' k8s/namespaces/$NAMESPACE/namespace.yaml | cut -f1 -d":")
-  HELM_RELEASE_NAME=$(yq eval '{.metadata.name:.[.name]}' $file | cut -f1 -d":")
+  FILE_NAMESPACE=$(yq eval '.metadata.name' k8s/namespaces/$NAMESPACE/namespace.yaml)
+  HELM_RELEASE_NAME=$(yq eval '.metadata.name' $file)
   FILE_DIRECTORY="k8s/namespaces/$NAMESPACE/$HELM_RELEASE_NAME/"
   BASE_MANIFEST="$FILE_DIRECTORY$HELM_RELEASE_NAME.yaml"
-  SPEC_RELEASE_NAME=$(yq eval '{.spec.releaseName:.[.releaseName]}' $file | cut -f1 -d":")
-  
+  SPEC_RELEASE_NAME=$(yq eval '.spec.releaseName' $file)
+
   # Make sure spec release name is matching helm release name
   [ "$HELM_RELEASE_NAME" == "$SPEC_RELEASE_NAME" ] || (echo "spec.releaseName not matching HelmRelease name for $HELM_RELEASE_NAME" && exit 1 ) 
   
@@ -21,6 +20,4 @@ for file in $(grep -lr "kind: HelmRelease" --exclude-dir={admin,monitoring,neuve
   
 #   # Check base Manifest exists
   [ -f $BASE_MANIFEST ] || (echo "$HELM_RELEASE_NAME base manifest not found" && exit 1 )
-  
-  
 done
