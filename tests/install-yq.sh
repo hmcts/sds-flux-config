fcce06f83acd44010309ef4cdd481cc5c6666be0
msl8r <72858053+msl8r@users.noreply.github.com>
2021-07-23 13:07:18 +0100

    debugging

--- tests/install-yq.sh
+++ tests/install-yq.sh
@@ -4,4 +4,4 @@ set -e
 VERSION=v4.6.1
 BINARY=yq_linux_amd64
 wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - | \
-  tar xz && mv ${BINARY} /usr/bin/yq
+  tar xz && sudo mv ${BINARY} /usr/bin/yq
