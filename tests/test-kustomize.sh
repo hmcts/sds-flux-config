73777451ae729893c3d4a1057df52888fc90b0f1
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-04-26 13:03:48 +0100

    DTSPO-6489 - fluxv2 ithc migration - pip (#1771)

--- tests/test-kustomize.sh
+++ tests/test-kustomize.sh
@@ -11,7 +11,6 @@ kustomizepaths=(
     k8s/environments/demo/common-overlay
     k8s/environments/ithc/cluster-00-overlay
     k8s/environments/ithc/cluster-01-overlay
-    k8s/environments/ithc/common-overlay
     k8s/environments/prod/cluster-00-overlay
     k8s/environments/prod/cluster-01-overlay
     k8s/environments/prod/common-overlay
