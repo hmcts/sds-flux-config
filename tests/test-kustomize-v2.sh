788a5c856e84e2d5b16d0e2068962ceec2e34bfc
Enda Kelly <enda.kelly@hmcts.net>
2022-07-20 16:21:34 +0100

    add image-repo and image-policy check to tests

--- tests/test-kustomize-v2.sh
+++ tests/test-kustomize-v2.sh
@@ -7,6 +7,31 @@ kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" -o install_kustomize
 ENVIRONMENTS="sbox ptlsbox ptl ithc dev test stg demo prod"
 EXCLUDE_APPS='example-exclude|flux-system'
 
+for FOLDER_FIND in $(find apps -type d -name "automation" | grep -Ev "$EXCLUDE_APPS"); do
+
+    IMAGE_POLICY_FILE=$(cat $FOLDER_FIND/kustomization.yaml | yq '.resources[] | select(. == "*image-policy.yaml")' | sed -r 's/.{2}//')
+    for path in ${IMAGE_POLICY_FILE[@]}; do
+
+        if [[ ! -f "$(dirname $FOLDER_FIND)$path" ]]
+            then
+                echo "Image policy file $(dirname $FOLDER_FIND)$path is missing" && exit 1
+            else
+                echo "Image policy file $(dirname $FOLDER_FIND)$path exists"
+        fi
+    done
+
+    IMAGE_REPO_FILE=$(cat $FOLDER_FIND/kustomization.yaml | yq '.resources[] | select(. == "*image-repo.yaml")' | sed -r 's/.{2}//')
+    for path in ${IMAGE_REPO_FILE[@]}; do
+
+        if [[ ! -f "$(dirname $FOLDER_FIND)$path" ]]
+            then
+                echo "Image policy file $(dirname $FOLDER_FIND)$path is missing" && exit 1
+            else
+                echo "Image policy file $(dirname $FOLDER_FIND)$path exists"
+        fi
+    done
+done
+
 for ENVIRONMENT in $(echo ${ENVIRONMENTS}); do
 kustomizepaths=()
 
@@ -27,6 +52,19 @@ kustomizepaths=()
         fi
 
     done
+
+        for path in "${kustomizepaths[@]}"; do
+        ./kustomize build --load_restrictor none $path >/dev/null
+        if [ $? -eq 1 ]
+        then
+        echo "Kustomize failing for env $path" && exit 1
+        fi
+        done
+
+done
+
+for ENVIRONMENT in $(echo ${ENVIRONMENTS}); do
+kustomizepaths=()
 
         for path in "${kustomizepaths[@]}"; do
         ./kustomize build --load_restrictor none $path >/dev/null
