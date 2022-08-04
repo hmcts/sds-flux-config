aadd6fe7d16ce8743cc32f3e05c8de644c7b31a1
msl8r <72858053+msl8r@users.noreply.github.com>
2022-08-04 11:03:58 +0100

    Update add-namespace-to-env.sh

--- bin/v2/add-namespace-to-env.sh
+++ bin/v2/add-namespace-to-env.sh
@@ -45,8 +45,6 @@ fi
 
 fi
 
-
-
 export NAMESPACE_PATH="../../../apps/$NAMESPACE/base/kustomize.yaml"
 if [[ $(yq eval '.resources[] | ( . == env(NAMESPACE_PATH))' ${CLUSTERS_DIR}/${ENVIRONMENT}/base/kustomization.yaml) =~ "true" ]]; then
   echo "Reference to ${CLUSTERS_DIR}/${ENVIRONMENT}/base/kustomization.yaml already exists ignoring.."
