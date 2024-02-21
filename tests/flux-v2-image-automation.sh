0dbca6c5a75d4bb44212440dadadd83107699547
nicholas chang <nicholasc001@hotmail.com>
2024-02-21 16:41:57 +0000

    add test image to fail

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -53,9 +53,10 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
 
             if [ $IMAGE_AUTOMATION_CHECK == false ]
             then
-                echo "Non whitelisted pattern found in ImagePolicy: $IMAGE_POLICY it should be ^prod-[a-f0-9]+-(?P<ts>[0-9]+)" && exit 1
+                echo "Non whitelisted pattern found in ImagePolicy: $IMAGE_POLICY it should be ^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
+                exit 1
             fi
-        done
+            done
 
     done
 
