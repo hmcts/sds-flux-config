5c795a7b30e335a9eea8c14fb88e2041e64bbb50
nicholas chang <nicholasc001@hotmail.com>
2024-02-22 09:48:00 +0000

    edit script

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x 
 set -ex -o pipefail
 set -x
 
@@ -59,32 +60,13 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
             IMAGE_AUTOMATION_CHECK=$(cat imagepolicies_list.yaml  | \
             IMAGE_POLICY_NAME="${IMAGE_POLICY}" yq eval 'select(.metadata and .kind == "ImagePolicy" and .metadata.name == env(IMAGE_POLICY_NAME) )' - | yq eval '.spec.filterTags.pattern == "^prod-[a-f0-9]+-(?P<ts>[0-9]+)"' -)
 
-            if [ $IMAGE_AUTOMATION_CHECK == false ]
-            then
-                echo "Non whitelisted pattern found in ImagePolicy: $IMAGE_POLICY it should be ^prod-[a-f0-9]+-(?P<ts>[0-9]+)" && exit 1
-            fi
+        if [ "$IMAGE_AUTOMATION_CHECK" == "false" ]
+        then
+            echo "Non whitelisted pattern found in ImagePolicy: $IMAGE_POLICY it should be ^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
+            exit 1
+        fi
         done
 
-        OUTPUTFILE="images.yaml"
-        DIRECTORIES=$(find apps -type d -not -path '*dev*' -not -path '*stg*' -not -path '*test*' -not -path '*ithc*' -not -path '*demo*' -not -path '*sbox*' -not -path 'apps' -not -path '*dynatrace*' -not -path '*azureserviceoperator-system*' -not -path '*flux-system*' -not -path '*neuvector*' -not -path '*traefik2*')
-
-        for dir in $DIRECTORIES; do
-            if ls "$dir" | grep -q -E 'kustomization\.ya?ml'; then
-
-                kustomize build --load-restrictor LoadRestrictionsNone "$dir" 2>&1 | yq eval 'select(.kind == "HelmRelease" and (.spec.values.nodejs.image != null or .spec.values.java.image != null))' >> $OUTPUTFILE
-
-                IMAGE_PATTERN="^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
-                nodejs_image=$(echo "$OUTPUTFILE" | yq eval '.spec.values.nodejs.image' -)
-                java_image=$(echo "$OUTPUTFILE" | yq eval '.spec.values.java.image' -)
-
-                extract_nodejs_image=$(echo $nodejs_image | cut -d ':' -f 2-)
-                extract_java_image=$(echo $java_image | cut -d ':' -f 2-)
-
-                if ! echo "\"$extract_nodejs_image\"" | jq --arg pattern "^prod-[a-f0-9]+-([0-9]+)" 'test($pattern)' > /dev/null || ! echo "\"$extract_java_image\"" | jq --arg pattern "^prod-[a-f0-9]+-([0-9]+)" 'test($pattern)'; then
-                    echo "image do not match"
-                    exit 1
-                fi
-            fi
-        done
     done
-done
+
+  done
