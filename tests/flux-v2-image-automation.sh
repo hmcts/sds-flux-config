086fe2214ff07cc9dcdc755208dd6a41debb0abc
nicholas chang <nicholasc001@hotmail.com>
2024-02-22 10:36:02 +0000

    add real image

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -52,14 +52,6 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
             IMAGE_AUTOMATION_CHECK=$(cat imagepolicies_list.yaml  | \
             IMAGE_POLICY_NAME="${IMAGE_POLICY}" yq eval 'select(.metadata and .kind == "ImagePolicy" and .metadata.name == env(IMAGE_POLICY_NAME) )' - | yq eval '.spec.filterTags.pattern == "^prod-[a-f0-9]+-(?P<ts>[0-9]+)"' -)
 
-            IMAGE_NAME=$(echo $IMAGE_AUTOMATION_CHECK | cut -d ':' -f2)
-
-            PATTERN="^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
-
-            if [[ ! $IMAGE_NAME =~ $PATTERN ]]; then
-                echo "Image name does not match the pattern: $IMAGE_NAME"
-                exit 1
-            fi
 
             if [ $IMAGE_AUTOMATION_CHECK == false ]
             then
