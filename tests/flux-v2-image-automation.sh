e5ea1d2ec2a2517868a1d9a3d6769153ebbbf6ac
nicholas chang <nicholasc001@hotmail.com>
2024-02-22 12:51:53 +0000

    Add wrong image

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -51,14 +51,21 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
             IMAGE_AUTOMATION_CHECK=$(cat imagepolicies_list.yaml  | \
             IMAGE_POLICY_NAME="${IMAGE_POLICY}" yq eval 'select(.metadata and .kind == "ImagePolicy" and .metadata.name == env(IMAGE_POLICY_NAME) )' - | yq eval '.spec.filterTags.pattern == "^prod-[a-f0-9]+-(?P<ts>[0-9]+)"' -)
 
-            PATTERN="^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
+                IMAGE_NAME=$(echo $IMAGE_AUTOMATION_CHECK | cut -d ':' -f2)
 
-            if [[ ! $IMAGE_AUTOMATION_CHECK =~ $PATTERN ]]; then
-                echo "Non whitelisted pattern found in ImagePolicy: $IMAGE_POLICY it should be ^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
-                exit 1
+                PATTERN="^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
+
+                if [[ ! $IMAGE_AUTOMATION_CHECK =~ $PATTERN ]]; then
+                    echo "Non whitelisted pattern found in ImagePolicy: $IMAGE_POLICY it should be ^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
+                    exit 1
+                fi
+
+            if [ $IMAGE_AUTOMATION_CHECK == false ]
+            then
+                echo "Non whitelisted pattern found in ImagePolicy: $IMAGE_POLICY it should be ^prod-[a-f0-9]+-(?P<ts>[0-9]+)" && exit 1
             fi
-
         done
+
     done
 
   done
