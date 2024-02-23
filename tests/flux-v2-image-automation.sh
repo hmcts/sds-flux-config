2f27108c3f4ac7d3842b6b8f2edc85b0d9f133ac
nicholas chang <nicholasc001@hotmail.com>
2024-02-23 16:28:27 +0000

    check script

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -ex -o pipefail
+# set -ex -o pipefail
 
 EXCLUSIONS_LIST=(
   apps/flux-system/*
@@ -29,7 +29,7 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
 
     done
 
-    ./kustomize build --load-restrictor LoadRestrictionsNone "clusters/ptl/base" | yq eval 'select(.metadata and .kind == "ImagePolicy")' -  > imagepolicies_list.yaml
+    kustomize build --load-restrictor LoadRestrictionsNone "clusters/ptl/base" | yq eval 'select(.metadata and .kind == "ImagePolicy")' -  > imagepolicies_list.yaml
     [ $? -eq 0 ] || (echo "Kustomize build has failed" && exit 1)
 
     for IMAGE_POLICY in "${IMAGE_POLICIES[@]}"; do
@@ -39,8 +39,10 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
         for path in $(echo "clusters/ptl/base"); do
 
         IMAGE_AUTOMATION=$(cat imagepolicies_list.yaml | \
-        IMAGE_POLICY_NAME="${IMAGE_POLICY}" yq eval 'select(.metadata and .kind == "ImagePolicy" and .metadata.name == env(IMAGE_POLICY_NAME) )' -)
+        IMAGE_POLICY_NAME="${vh-admin-web-dev}" yq eval 'select(.metadata and .kind == "ImagePolicy" and .metadata.name == env(IMAGE_POLICY_NAME) )' -)
 
+
+        # cat imagepolicies_list.yaml | yq eval 'select(.metadata and .kind == "ImagePolicy" and .metadata.name == "vh-admin-web-dev")' -
 
 
             if [ "$IMAGE_AUTOMATION" == "" ]
@@ -48,13 +50,18 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
                 echo "No ImagePolicy for $IMAGE_POLICY in clusters/ptl-intsvc/base" && exit 1
             fi
 
-            IMAGE_AUTOMATION_CHECK=$(cat imagepolicies_list.yaml  | \
-            IMAGE_POLICY_NAME="${IMAGE_POLICY}" yq eval 'select(.metadata and .kind == "ImagePolicy" and .metadata.name == env(IMAGE_POLICY_NAME) )' - | yq eval '.spec.filterTags.pattern | "^prod-[a-f0-9]+-(?P<ts>[0-9]+)"' -)
+            # IMAGE_AUTOMATION_CHECK=$(cat imagepolicies_list.yaml  | \
+            # # IMAGE_POLICY_NAME="${IMAGE_POLICY}" yq eval 'select(.metadata and .kind == "ImagePolicy" and .metadata.name == env(IMAGE_POLICY_NAME) )' - | yq eval '.spec.filterTags.pattern | "^prod-[a-f0-9]+-(?P<ts>[0-9]+)"' -)
+            # IMAGE_POLICY_NAME="${IMAGE_POLICY}" yq eval 'select(.metadata and .kind == "ImagePolicy" and .metadata.name == env(IMAGE_POLICY_NAME) )' - | yq eval '.spec.filterTags.pattern | test("^prod-[a-f0-9]+-(?P<ts>[0-9]+)")' -)
 
-                if [[ $IMAGE_AUTOMATION_CHECK == "false" ]]; then
-                    echo "Non whitelisted pattern found in ImagePolicy: $IMAGE_POLICY it should be ^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
-                    exit 1
-                fi
+            #     if [[ $IMAGE_AUTOMATION_CHECK == "true" ]]; then
+            #         echo "Non whitelisted pattern found in ImagePolicy: $IMAGE_POLICY it should be ^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
+            #         exit 1
+            #     fi
+            IMAGE_POLICY_NAME="juror-api"
+            IMAGE_AUTOMATION_CHECK=$(cat imagepolicies_list.yaml  | \
+            yq eval 'select(.metadata and .kind == "ImagePolicy" and .metadata.name == env(IMAGE_POLICY_NAME) )' - | yq eval '.spec.filterTags.pattern | test("^prod-[a-f0-9]+-(?P<ts>[0-9]+)")' -)
+            echo $IMAGE_AUTOMATION_CHECK
             done
 
     done
