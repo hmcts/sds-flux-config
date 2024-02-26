615ffb3047fdc71f4ebe108b11d7767113e7e1a1
nicholas chang <nicholasc001@hotmail.com>
2024-02-26 16:21:34 +0000

    edit file directory

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -59,7 +59,7 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
 
             IMAGE_REPOSITORY_REF=$(yq e '.spec.imageRepositoryRef.name' imagepolicies_list.yaml)
             PATTERN="^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
-            DIR="sdshmctspublic.azurecr.io/juror/api:prod-a6bc219-20240226123038"
+            DIR="./apps/juror/juror-api/juror-api.yaml"
 
             for file in $DIR/*.yaml; do
 
