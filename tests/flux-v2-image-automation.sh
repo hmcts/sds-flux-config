203e462569f36332e5adf3b74f11857c8ac15588
nicholas chang <nicholasc001@hotmail.com>
2024-02-26 17:37:39 +0000

    edit vairable

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -59,7 +59,7 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
 
             # IMAGE_REPOSITORY_REF=$(yq e '.spec.imageRepositoryRef.name' imagepolicies_list.yaml)
             PATTERN="^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
-            # DIR="clusters/prod/base"
+            # FILE="clusters/prod/base"
             FILE="./apps/juror/juror-api/juror-api.yaml"
 
             # for file in $DIR/*.yaml; do
