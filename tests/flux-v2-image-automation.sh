9c4320537bc6f7876bce360418b3c211f1e4c599
nicholas chang <nicholasc001@hotmail.com>
2024-02-26 16:09:50 +0000

    add correct image

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -59,7 +59,7 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
 
             IMAGE_REPOSITORY_REF=$(yq e '.spec.imageRepositoryRef.name' imagepolicies_list.yaml)
             PATTERN="^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
-            DIR="clusters/ptl/base"
+            DIR="clusters/prod/base"
 
             for file in $DIR/*.yaml; do
 
