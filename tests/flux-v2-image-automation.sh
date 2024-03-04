0f1ff1c9fef52ff6e7a8ed7259c63eac46589d9a
nicholas chang <nicholasc001@hotmail.com>
2024-03-04 16:11:22 +0000

    add back exclusion variable

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -65,7 +65,7 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
         done
 
         OUTPUTFILE="images.yaml"
-        DIRECTORIES=$(find "$FILE_LOCATIONS" -type d -not -path "$EXCLUSIONS")
+        DIRECTORIES=$(find cd /apps -type d -not -path "$EXCLUSIONS")
 
         for dir in $DIRECTORIES; do
 
