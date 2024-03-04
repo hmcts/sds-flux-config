66d3d519613f9466781acd1b83ff0b322d4010a1
nicholas chang <nicholasc001@hotmail.com>
2024-03-04 16:53:55 +0000

    fix path

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -70,8 +70,7 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
         for dir in $DIRECTORIES; do
 
             echo "Checking HelmRelease in $dir"
-            kustomize build --load-restrictor LoadRestrictionsNone "$dir" | yq eval 'select(.kind == "HelmRelease" and (.spec.values.nodejs.image != null or .spec.values.java.image != null))' > $OUTPUTFILE
-
+            kustomize build --load-restrictor LoadRestrictionsNone $GITHUB_WORKSPACE/"$dir" | yq eval 'select(.kind == "HelmRelease" and (.spec.values.nodejs.image != null or .spec.values.java.image != null))' >> $OUTPUTFILE
             # IMAGE_PATTERN="^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
 
             # while read -r output; do
