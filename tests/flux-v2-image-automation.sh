b26d7a71cb7c259eadc674f8760f89b4d3028dd1
nicholas chang <nicholasc001@hotmail.com>
2024-03-04 09:59:08 +0000

    Add exit 1

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -63,6 +63,7 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
         for dir in $DIRECTORIES; do
             find "$dir" -name "prod.yaml" -name "base.yaml" |
             kustomize build --load-restrictor LoadRestrictionsNone "$dir" 2>&1 | yq eval 'select(.kind == "HelmRelease" and (.spec.values.nodejs.image != null or .spec.values.java.image != null))' >> $OUTPUTFILE
+            exit 1
         done
     done
 
