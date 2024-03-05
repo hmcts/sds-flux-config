4ed7e2dd1326f9d165c60db4dd8351789fc42f07
nicholas chang <nicholasc001@hotmail.com>
2024-03-05 10:57:23 +0000

    test working directory path

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -65,7 +65,7 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
         done
 
         OUTPUTFILE="images.yaml"
-        DIRECTORIES=$(find "apps" -type d -not -path "$EXCLUSIONS")
+        DIRECTORIES=$(find $workingdirectory/apps -type d -not -path "$EXCLUSIONS")
 
         for dir in $DIRECTORIES; do
 
