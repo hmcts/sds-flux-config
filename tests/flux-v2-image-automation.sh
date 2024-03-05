a1af49f9c1f618bb51d64d4832965f9401c0b981
nicholas chang <nicholasc001@hotmail.com>
2024-03-05 12:00:19 +0000

    test script

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -82,6 +82,6 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
             #         echo "Error: No match found for image pattern in line: $output"
             #     fi
             # done < $OUTPUTFILE
-        done
+        # done
     done
 done
