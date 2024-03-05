3d78a3a11fe00eda48dbbb3bc568792609c576c2
nicholas chang <nicholasc001@hotmail.com>
2024-03-05 12:56:41 +0000

    edit directories path to exclude

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -65,7 +65,7 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
         done
 
         OUTPUTFILE="images.yaml"
-        DIRECTORIES=$(find $FILE_LOCATIONS -type d -not -path "$EXCLUSIONS")
+        DIRECTORIES=$(find apps -type d -not -path '*dev*' -not -path '*stg*' -not -path '*test*' -not -path '*ithc*' -not -path '*demo*' -not -path '*sbox*' -not -path 'apps')
 
         for dir in $DIRECTORIES; do
 
