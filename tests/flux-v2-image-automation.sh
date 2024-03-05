ec6bd730678fe5b88aa7bb8b5092825f7d987350
nicholas chang <nicholasc001@hotmail.com>
2024-03-05 13:06:21 +0000

    add another > to append to output

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -65,7 +65,7 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
         done
 
         OUTPUTFILE="images.yaml"
-        DIRECTORIES=$(find apps -type d -not -path '*dev*' -not -path '*stg*' -not -path '*test*' -not -path '*ithc*' -not -path '*demo*' -not -path '*sbox*' -not -path 'apps')
+        DIRECTORIES=$(find apps -type d -not -path '*dev*' -not -path '*stg*' -not -path '*test*' -not -path '*ithc*' -not -path '*demo*' -not -path '*sbox*' -not -path 'apps' -not -path  '*toffee*')
 
         for dir in $DIRECTORIES; do
 
