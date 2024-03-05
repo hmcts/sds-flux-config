de768486a91129c84352e43ed3923a10b2525051
nicholas chang <nicholasc001@hotmail.com>
2024-03-05 15:11:47 +0000

    fix kustomize command

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -71,17 +71,6 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
         for dir in $DIRECTORIES; do
 
             ./kustomize build --load-restrictor LoadRestrictionsNone "$dir" 2>&1 | yq eval 'select(.kind == "HelmRelease" and (.spec.values.nodejs.image != null or .spec.values.java.image != null))' >> $OUTPUTFILE
-
-            # IMAGE_PATTERN="^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
-
-            # while read -r output; do
-            #     nodejs_image=$(echo "$output" | yq eval '.spec.values.nodejs.image' -)
-            #     java_image=$(echo "$output" | yq eval '.spec.values.java.image' -)
-
-            #     if [[ "$nodejs_image" && "$java_image" != $IMAGE_PATTERN ]]; then
-            #         echo "Error: No match found for image pattern in line: $output"
-            #     fi
-            # done < $OUTPUTFILE
         done
     done
 done
