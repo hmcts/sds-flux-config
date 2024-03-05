cf9acb0b46b38052d249dd9487ab2541efaef2e4
Rees Pozzi <reespozzi@icloud.com>
2024-03-05 15:50:18 +0000

    Testing something

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -1,62 +1,73 @@
 #!/usr/bin/env bash
 set -ex -o pipefail
 
-EXCLUSIONS_LIST=(
-  apps/flux-system/*
-  apps/vh/*/stg.yaml
-  .*demo.*.yaml
-  .*test.*.yaml
-  .*ithc.*.yaml
-  .*sbox.*.yaml
-  .*dev.*.yaml
-)
+OUTPUTFILE="images.yaml"
+DIRECTORIES=$(find apps -type d -not -path '*dev*' -not -path '*stg*' -not -path '*test*' -not -path '*ithc*' -not -path '*demo*' -not -path '*sbox*' -not -path 'apps' -not -path '*dynatrace*' -not -path '*azureserviceoperator-system*' -not -path '*flux-system*' -not -path '*neuvector*')
 
-EXCLUSIONS=$(IFS="|" ; echo "${EXCLUSIONS_LIST[*]}")
-FILE_LOCATIONS="apps"
+echo $DIRECTORIES
+for dir in $DIRECTORIES; do
+    if ls "$dir" | grep -q -E 'kustomization\.ya?ml'; then
+        echo "Checking HelmRelease in $dir"
+        kustomize build --load-restrictor LoadRestrictionsNone "$dir"| yq eval 'select(.kind == "HelmRelease" and (.spec.values.nodejs.image != null or .spec.values.java.image != null))' >> $OUTPUTFILE
+    fi
+done
 
-for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
+# EXCLUSIONS_LIST=(
+#   apps/flux-system/*
+#   apps/vh/*/stg.yaml
+#   .*demo.*.yaml
+#   .*test.*.yaml
+#   .*ithc.*.yaml
+#   .*sbox.*.yaml
+#   .*dev.*.yaml
+# )
 
-    IMAGE_POLICIES=()
-    for FILE in $(grep -lr "imagepolicy" $FILE_LOCATION | grep -Ev "$EXCLUSIONS" ); do
+# EXCLUSIONS=$(IFS="|" ; echo "${EXCLUSIONS_LIST[*]}")
+# FILE_LOCATIONS="apps"
 
-        if [ $(yq eval '.kind' $FILE) != "HelmRelease" ]
-        then
-            continue
-        fi
+# for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
 
-        IFS=$'\n'
-        IMAGE_POLICIES+=($(grep -o "flux-system:.*" $FILE | cut -d ':' -f2 | sed 's/..$//'))
+#     IMAGE_POLICIES=()
+#     for FILE in $(grep -lr "imagepolicy" $FILE_LOCATION | grep -Ev "$EXCLUSIONS" ); do
 
-    done
+#         if [ $(yq eval '.kind' $FILE) != "HelmRelease" ]
+#         then
+#             continue
+#         fi
 
-    ./kustomize build --load-restrictor LoadRestrictionsNone "clusters/ptl/base" | yq eval 'select(.metadata and .kind == "ImagePolicy")' -  > imagepolicies_list.yaml
-    [ $? -eq 0 ] || (echo "Kustomize build has failed" && exit 1)
+#         IFS=$'\n'
+#         IMAGE_POLICIES+=($(grep -o "flux-system:.*" $FILE | cut -d ':' -f2 | sed 's/..$//'))
 
-    for IMAGE_POLICY in "${IMAGE_POLICIES[@]}"; do
+#     done
 
-        echo "Checking image policy: $IMAGE_POLICY"
+#     ./kustomize build --load-restrictor LoadRestrictionsNone "clusters/ptl/base" | yq eval 'select(.metadata and .kind == "ImagePolicy")' -  > imagepolicies_list.yaml
+#     [ $? -eq 0 ] || (echo "Kustomize build has failed" && exit 1)
 
-        for path in $(echo "clusters/ptl/base"); do
+#     for IMAGE_POLICY in "${IMAGE_POLICIES[@]}"; do
 
-        IMAGE_AUTOMATION=$(cat imagepolicies_list.yaml | \
-        IMAGE_POLICY_NAME="${IMAGE_POLICY}" yq eval 'select(.metadata and .kind == "ImagePolicy" and .metadata.name == env(IMAGE_POLICY_NAME) )' -)
+#         echo "Checking image policy: $IMAGE_POLICY"
+
+#         for path in $(echo "clusters/ptl/base"); do
+
+#         IMAGE_AUTOMATION=$(cat imagepolicies_list.yaml | \
+#         IMAGE_POLICY_NAME="${IMAGE_POLICY}" yq eval 'select(.metadata and .kind == "ImagePolicy" and .metadata.name == env(IMAGE_POLICY_NAME) )' -)
 
 
 
-            if [ "$IMAGE_AUTOMATION" == "" ]
-            then
-                echo "No ImagePolicy for $IMAGE_POLICY in clusters/ptl-intsvc/base" && exit 1
-            fi
+#             if [ "$IMAGE_AUTOMATION" == "" ]
+#             then
+#                 echo "No ImagePolicy for $IMAGE_POLICY in clusters/ptl-intsvc/base" && exit 1
+#             fi
 
-            IMAGE_AUTOMATION_CHECK=$(cat imagepolicies_list.yaml  | \
-            IMAGE_POLICY_NAME="${IMAGE_POLICY}" yq eval 'select(.metadata and .kind == "ImagePolicy" and .metadata.name == env(IMAGE_POLICY_NAME) )' - | yq eval '.spec.filterTags.pattern == "^prod-[a-f0-9]+-(?P<ts>[0-9]+)"' -)
+#             IMAGE_AUTOMATION_CHECK=$(cat imagepolicies_list.yaml  | \
+#             IMAGE_POLICY_NAME="${IMAGE_POLICY}" yq eval 'select(.metadata and .kind == "ImagePolicy" and .metadata.name == env(IMAGE_POLICY_NAME) )' - | yq eval '.spec.filterTags.pattern == "^prod-[a-f0-9]+-(?P<ts>[0-9]+)"' -)
 
-            if [ $IMAGE_AUTOMATION_CHECK == false ]
-            then
-                echo "Non whitelisted pattern found in ImagePolicy: $IMAGE_POLICY it should be ^prod-[a-f0-9]+-(?P<ts>[0-9]+)" && exit 1
-            fi
-        done
+#             if [ $IMAGE_AUTOMATION_CHECK == false ]
+#             then
+#                 echo "Non whitelisted pattern found in ImagePolicy: $IMAGE_POLICY it should be ^prod-[a-f0-9]+-(?P<ts>[0-9]+)" && exit 1
+#             fi
+#         done
 
-    done
+#     done
 
-  done
+#   done
