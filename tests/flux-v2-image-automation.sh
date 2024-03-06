f4dffba82864e55e6b1ae10ea0c20f5c05044c24
nicholas chang <nicholasc001@hotmail.com>
2024-03-06 16:24:51 +0000

    edit working script

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -71,20 +71,20 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
         for dir in $DIRECTORIES; do
             if ls "$dir" | grep -q -E 'kustomization\.ya?ml'; then
 
-                echo "Checking Directory $dir"
                 kustomize build --load-restrictor LoadRestrictionsNone "$dir" 2>&1 | yq eval 'select(.kind == "HelmRelease" and (.spec.values.nodejs.image != null or .spec.values.java.image != null))' >> $OUTPUTFILE
 
+                IMAGE_PATTERN="^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
+                nodejs_image=$(echo "$OUTPUTFILE" | yq eval '.spec.values.nodejs.image' -)
+                java_image=$(echo "$OUTPUTFILE" | yq eval '.spec.values.java.image' -)
+
+                extract_nodejs_image=$(echo $nodejs_image | cut -d ':' -f 2-)
+                extract_java_image=$(echo $java_image | cut -d ':' -f 2-)
+
+                if ! echo "\"$extract_nodejs_image\"" | jq --arg pattern "^prod-[a-f0-9]+-([0-9]+)" 'test($pattern)' > /dev/null || ! echo "\"$extract_java_image\"" | jq --arg pattern "^prod-[a-f0-9]+-([0-9]+)" 'test($pattern)'; then
+                    echo "image do not match"
+                    exit 1
+                fi
             fi
-            # IMAGE_PATTERN="^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
-
-            # while read -r output; do
-            #     nodejs_image=$(echo "$output" | yq eval '.spec.values.nodejs.image' -)
-            #     java_image=$(echo "$output" | yq eval '.spec.values.java.image' -)
-
-            #     if [[ "$nodejs_image" && "$java_image" != $IMAGE_PATTERN ]]; then
-            #         echo "Error: No match found for image pattern in line: $output"
-            #     fi
-            # done < $OUTPUTFILE
         done
     done
 done
