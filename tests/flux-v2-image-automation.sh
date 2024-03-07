e94e499960a1352accb0e1ab1b7f1711d7ec1387
nicholas chang <nicholasc001@hotmail.com>
2024-03-07 12:57:59 +0000

    compare with martin code

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -2,15 +2,11 @@
 # set -ex -o pipefail
 
 EXCLUSIONS_LIST=(
-    apps/flux-system/*
-    apps/my-time/my-time-frontend/my-time-frontend.yaml
-    apps/met/themis-fe/prod.yaml
-    apps/aspnet/dotnet48/dotnet48.yaml
-    apps/hmi/hmi-rota-dtu/hmi-rota-dtu.yaml
-    apps/juror-digital/jd-public/*
-    apps/juror-digital/jd-bureau/*
-    apps/juror-digital/moj-reverse-proxy/*
-    apps/jenkins/jenkins/*
+    # apps/flux-system/*
+    # apps/my-time/my-time-frontend/my-time-frontend.yaml
+    # apps/met/themis-fe/prod.yaml
+    # apps/aspnet/dotnet48/dotnet48.yaml
+    # apps/hmi/hmi-rota-dtu/hmi-rota-dtu.yaml
     .*perftest.*
     .*sbox.*
     .*test.*
@@ -29,13 +25,6 @@ FILE_LOCATIONS="apps"
 
 for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
 
-    ##############################################################################################################
-    # This section compiles a list of files that contains `imagepolicy` and stores them in an array IMAGE_POLICIES
-    # Only files that follow these rules will be added to the array:
-    # - Contains the `imagepolicy` string 
-    # - Is NOT in the exclusions list
-    # - Is not HelmRelease type document
-    ##############################################################################################################
     IMAGE_POLICIES=()
     for FILE in $(grep -lr "imagepolicy" $FILE_LOCATION | grep -Ev "$EXCLUSIONS" ); do
 
@@ -50,21 +39,9 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
 
     done
 
-    ##############################################################################################################
-    # This section will scan the PTL cluster to find all ImagePolicy configs and add them to temporary yaml file
-    # Only scans `clusters/ptl/base` because thats where Image Policies are found, scanning other clusters will
-    # result in no output
-    ##############################################################################################################
     ./kustomize build --load-restrictor LoadRestrictionsNone "clusters/ptl/base" | yq eval 'select(.metadata and .kind == "ImagePolicy")' -  > imagepolicies_list.yaml
     [ $? -eq 0 ] || (echo "Kustomize build has failed" && exit 1)
 
-    ##############################################################################################################
-    # This section loops over each file in the IMAGE_POLICIES array and compares the file with the documents added 
-    # to the temporary file imagepolicies_list.yaml
-    # If a document is found in the temporary file with the matching name of a policy it is then checked to see
-    # if its pattern value matches the specified Prod regex.
-    # If this results in a false (i.e. it doesnt match) the script will fail and print the offending Policy name.
-    ##############################################################################################################
     for IMAGE_POLICY in "${IMAGE_POLICIES[@]}"; do
 
         for path in $(echo "clusters/ptl/base"); do
@@ -74,7 +51,7 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
 
             if [ "$IMAGE_AUTOMATION" == "" ]
             then
-                echo "No ImagePolicy for $IMAGE_POLICY in clusters/ptl/base" && exit 1
+                echo "No ImagePolicy for $IMAGE_POLICY in clusters/ptl-intsvc/base" && exit 1
             fi
 
             IMAGE_AUTOMATION_CHECK=$(cat imagepolicies_list.yaml  | \
@@ -120,7 +97,6 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
             if ls "$dir" | grep -q -E 'kustomization\.ya?ml'; then
                 kustomize build --load-restrictor LoadRestrictionsNone "$dir" 2>&1 | yq eval 'select(.kind == "HelmRelease" and (.spec.values.nodejs.image != null or .spec.values.java.image != null))' >> $OUTPUTFILE
             fi
-        done
 
         for OUTPUTFILE in "${HELMRELEASES[@]}"; do
                 IMAGE_PATTERN="^prod-[a-f0-9]+-(?P<ts>[0-9]+)"
@@ -132,6 +108,7 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
                     echo "!! Non whitelisted pattern found in HelmRelease: $OUTPUTFILE it should be prod-[a-f0-9]+-(?P<ts>[0-9]+)" && exit 1
                     exit 1
                 fi
+        done
     done
     ##############################################################################################################
     # Print success if ALL Helm Release image fields are valid
