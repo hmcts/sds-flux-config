39a5856674d85784631dabde4394efba400d8e4a
nicholas chang <nicholasc001@hotmail.com>
2024-03-08 15:57:27 +0000

    add while loop

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -76,27 +76,26 @@ for FILE_LOCATION in $(echo ${FILE_LOCATIONS}); do
     # - Is HelmRelease type document
     ##############################################################################################################
 
-    HELMRELEASES=()
-    for FILE in $(grep -lr "image:" $FILE_LOCATION | grep -Ev "$EXCLUSIONS" ); do
-        while read -r doc; do
-            if [ "$doc" == "HelmRelease" ]; then
-                IFS=$'\n'
-                HELMRELEASES+=("$FILE")
-            fi
-        done < <(yq eval '.kind' $FILE)
-    done
+HELMRELEASES=()
+for FILE in $(grep -lr "image:" $FILE_LOCATION | grep -Ev "$EXCLUSIONS" ); do
+    while read -r doc; do
+        if [ "$doc" == "HelmRelease" ]; then
+            IFS=$'\n'
+            HELMRELEASES+=("$FILE")
+        fi
+    done < <(yq eval '.kind' $FILE)
+done
 
         for RELEASE in "${HELMRELEASES[@]}"; do
             image=$(yq eval 'select(.spec.values.image) or (.spec.values.*.image) != null' $RELEASE)
             extracted_image=$(echo $image | cut -d ':' -f 2-)
 
             while read -r doc; do
-                if [ "$doc" == false ]; then
+                if [ "$doc" == "false" ]; then
                     echo "!! Non whitelisted pattern found in HelmRelease: $RELEASE it should be prod-[a-f0-9]+-(?P<ts>[0-9]+)" && exit 1
                 fi
-            done < <(yq eval 'test("^prod-[a-f0-9]+-([0-9]+)")' <<< "$extracted_image")
+            done < <(yq eval 'tostring(test("^prod-[a-f0-9]+-([0-9]+)"))' <<< "$extracted_image")
         done
-  done
     ##############################################################################################################
     # Print success if ALL Helm Release image fields are valid
     ##############################################################################################################
