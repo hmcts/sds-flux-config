cd7fc60caede6813e51b7d60b06b328809b7bcab
nicholas chang <nicholasc001@hotmail.com>
2024-03-08 16:16:07 +0000

    add 2>&1

--- tests/flux-v2-image-automation.sh
+++ tests/flux-v2-image-automation.sh
@@ -94,7 +94,7 @@ done
                 if [ "$doc" == "false" ]; then
                     echo "!! Non whitelisted pattern found in HelmRelease: $RELEASE it should be prod-[a-f0-9]+-(?P<ts>[0-9]+)" && exit 1
                 fi
-            done < <(yq eval '(.spec.values.image) or (.spec.values.*.image) | test("prod-[a-f0-9]+-(?P<ts>[0-9]+)")' $extracted_image)
+            done < <(yq eval 'test("prod-[a-f0-9]+-(?P<ts>[0-9]+)")' <<< "$extracted_image")
     done
     ##############################################################################################################
     # Print success if ALL Helm Release image fields are valid
